6353b9f6426a5d3e73afc2dd5e453697a404f9c9
Sai <nosaitx3@gmail.com>
2017-01-12 16:58:10 +0700

    lier

--- wyj/urls.py
+++ wyj/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-
+from wyj.views import hello
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^hello/$',hello),
 ]
